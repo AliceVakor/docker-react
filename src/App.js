7db56aa336389782ede7084d90b005aeb683f1b2
AliceVakor <varvaradialog@gmail.com>
2021-10-03 14:29:00 +0200

    testing ci setup

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Hello, this is me, giraf!!! here you go!
+          Hello, this is me, giraf!!! here you go
         </p>
         <a
           className="App-link"
